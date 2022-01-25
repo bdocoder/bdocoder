23f0ddecd8990273cc545f73ce906267521ac6d5
Abdullah Ahmad <bdocoder@gmail.com>
2022-01-25 19:19:41 +0200

    feat(femio): made a script to print attribution info in frontend mentor challenges
    this script is to be included with a <script> tag in the end of a challenge html file, and attribution info will be printed in the console tab

---
+++ femio/attribution.js
@@ -0,0 +1,8 @@
+const attribution = `
+Challenge by Frontend Mentor
+> https://www.frontendmentor.io?ref=challenge
+Coded by Abdullah Ahmad (bdocoder)
+> https://www.frontendmentor.io/profile/bdocoder
+`.trim();
+
+console.log(attribution);
