bb2ee7358b7abe809995b14e5f8f2f5534811521
Abdullah Ahmad <bdocoder@gmail.com>
2024-12-20 13:16:21 +0200

    remove 50projects50days attribution

--- attributions/50projects50days.js
+++
@@ -1,6 +0,0 @@
-const attribution = `
-Project inspiration: https://github.com/bradtraversy/50projects50days
-Coded by Abdullah Ahmad (bdocoder): https://github.com/bdocoder
-`.trim();
-
-console.log(attribution);
